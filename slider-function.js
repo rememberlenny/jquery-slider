614750553b93cfcb9475a2b0ca7294ee03b1587b
rememberlenny <lkbgift@gmail.com>
2013-07-22 15:31:49 -0400

    added content swap

--- slider-function.js
+++ slider-function.js
@@ -1,11 +1,11 @@
 
   // Save variables to modify
   var SliderVar = {
-      sliderHead: jQuery('.slider-head-link'),
-      sliderContent: jQuery('.panel-contexts'),
+      sliderHead: jQuery('.slider-head-link li'),
+      sliderContent: jQuery('.brand-primary-bg div'),
       currentSliderHead: jQuery(this.slideHead).find('.active'),
-      currentSliderContent: jQuery(this.slideContent).find('.active'),
-      currentSlider: 0,
+      currentSliderContent: jQuery('.brand-primary-bg div.active').index('.brand-primary-bg div'),
+      currentSlider: jQuery('.slider-head-link li.active').index('.slider-head-link li'),
       slideTotalCount: jQuery('.slider-head-link').find('li').size() - 1
     };
   
@@ -15,8 +15,12 @@
     // Create functions
     inititalize: function(){
       console.log('Slider initialized.');
-      this.loadSlide();
       this.autoCycle();
+    },
+    autoCycle: function(){
+      console.log('Autocycle begin.');
+      this.passTime();
+      this.selectNext();
     },
     checkIfLastSlide: function(){
       if (SliderVar.currentSlider == SliderVar.slideTotalCount){
@@ -30,11 +34,13 @@
       console.log('Slide count reset.');
     },
     selectNext: function(){
+
       if (this.checkIfLastSlide != false){
         this.incrementSlide();
       } else {
         this.resetSlideCount();
       };
+      
       this.loadSlide(SliderVar.currentSlider);
       console.log('Next slide selected.');
     },
@@ -46,22 +52,34 @@
       SliderVar.currentSlider == 0;
       console.log('Slide count '+ SliderVar.currentSlider +'.');
     },
-    autoCycle: function(){
-      console.log('Autocycle begin.');
-      this.passTime();
-      this.selectNext();  
-    },
-    loadSlide: function(){;
+    loadSlide: function(){
       console.log('Slide load begun.');
       SliderVar.currentSliderHead = jQuery(this).find(SliderVar.sliderHead); 
       SliderVar.currentSliderContent = jQuery(this).find(SliderVar.sliderContent); 
       
       SliderVar.sliderHead.find('.active').removeClass('active');
-      SliderVar.currentSliderHead.addClass('active');
-      SliderVar.sliderContent.find('.active').removeClass('active');
-      SliderVar.currentSliderHead.addClass('active');
+    },
+    removeSlideClass: function(){
+      console.log('Previous slide head was ' + SliderVar.currentSlider + '.');
+      SliderVar.sliderHead.removeClass('active');
+    },
+    updateSlideCount: function(){
+      SliderVar.currentSlider = jQuery('.slider-head-link li.active').index('.slider-head-link li');
+    },
+    notifyCurrentSlide: function(){
+      this.updateSlideCount();
+      console.log('Current slide head is now ' + SliderVar.currentSlider + '.');
     },
     passTime: function(){
+    },
+    resetSlideContent: function () {
+      SliderVar.sliderContent.removeClass('active');
+    },
+    applyActiveSlideContent: function () {
+      jQuery(SliderVar.sliderContent).eq(SliderVar.currentSlider).addClass('active'); 
+    },
+    activateActiveSlideContent: function () {
+      // body...
     }
   };
   // Activate
@@ -69,10 +87,13 @@
   jQuery(function(jQuery){
     sliderApp.inititalize();
     sliderApp.autoCycle();
-  
+    
     jQuery(SliderVar.sliderHead).click(function(event){
-      alert( event.currentTarget === this);
+      sliderApp.removeSlideClass();
+      jQuery(this).addClass('active');
+      sliderApp.notifyCurrentSlide();
     });
+
   });
 
 
